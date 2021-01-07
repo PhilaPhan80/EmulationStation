f5d0683dc2793cd6423e62940a297471ec067627
PhilaPhan80 <53459459+PhilaPhan80@users.noreply.github.com>
2021-01-06 21:01:53 -0500

    Debugging

--- es-app/src/guis/GuiGamelistOptions.cpp
+++ es-app/src/guis/GuiGamelistOptions.cpp
@@ -122,7 +122,7 @@ GuiGamelistOptions::GuiGamelistOptions(Window* window, SystemData* system) : Gui
 		//addSaveFunc([selectedSortType] { Settings::getInstance()->setInt("SortType", mListSort->getValue()); });
 		addSaveFunc([mListSort] { 
 			LOG(LogInfo) << "addSaveFunc INNER 1";
-			Settings::getInstance()->setInt("SortType", mListSort->getSelected()->id); 
+			Settings::getInstance()->setInt("SortType", *mListSort->getSelected()->id); 
 			LOG(LogInfo) << "addSaveFunc INNER 2";
 			});
 
