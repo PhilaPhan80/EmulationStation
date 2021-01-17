8584d655f4f111e8da2db2ba136f55f3df08e28c
PhilaPhan80 <53459459+PhilaPhan80@users.noreply.github.com>
2021-01-16 21:38:43 -0500

    Debugging

--- es-app/src/FileData.cpp
+++ es-app/src/FileData.cpp
@@ -260,7 +260,7 @@ void FileData::sort(ComparisonFunction& comparator, bool ascending)
 
 	
 	auto fd = mChildren.cbegin();
-	if((*fd)->getChildren().size() > 0) {
+	if(1==1) {  //(*fd)->getChildren().size() > 0) {
 		//LOG(LogInfo) << "SORT: " + (*fd)->getName();
 		LOG(LogInfo) << "SORT: (NAME)";
 	}
